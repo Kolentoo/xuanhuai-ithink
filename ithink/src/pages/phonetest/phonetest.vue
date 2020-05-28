ef6728fa9e73b29339214dbf155f900703c4f9be
Kolento <515835543@qq.com>
2020-05-28 09:56:06 +0800

    add test page
    Signed-off-by: Kolento <515835543@qq.com>

---
+++ ithink/src/pages/phonetest/phonetest.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="phone-test">
+        <input type="text" v-model="phone">
+        <p @click="phoneCall">拨打</p>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+        phoneCall(){
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
